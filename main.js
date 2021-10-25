f2d5e072a7789f99660bdc71bc1d774ab25d7e9c
mcleodheads <vamaklakov@mmtr.ru>
2021-10-25 10:06:18 +0300

    fixes

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ let describe = mocha.describe
 let it = mocha.it
 let math = require('mathjs')
 
+
 // 1 factorial
 function factorial(num) {
     if (num < 0 || isNaN(num)) {
@@ -18,6 +19,97 @@ function factorial(num) {
 
 const arrFactorial = num => num <= 1 ? 1 : num * arrFactorial(num - 1)
 // FACTORIAL TESTS
+
+
+
+// 2 longest string
+function longWord(str) {
+    const stringsArr = str.split(' ').sort((a, b) => a.length - b.length)
+    return stringsArr[stringsArr.length - 1].length
+}
+
+const arrLongWord = str => str.split(' ').reduce((accumulate, current) => Math.max(current.length, accumulate), 0)
+
+
+
+// 3 biggest num
+function biggestNum(arr) {
+    return arr.map(innerArr => innerArr.reduce((accumulate, current) => Math.max(accumulate, current)))
+}
+
+const arrBiggestNum = arr => arr.map(innerArr => innerArr.reduce((accumulate, current) => Math.max(accumulate, current)))
+
+
+// 4 string max length
+function stringLength(str, num) {
+    return str.slice(0, num) + '...'
+}
+const arrStringLength = (str, num) => str.slice(0, num) + '...'
+
+
+
+
+// 5 Register
+function changeRegister(str) {
+    return str
+        .split(' ').map(word => word.toLowerCase().replace(/^./, word => word.toUpperCase())).join(' ')
+}
+
+const arrChangeRegister = str => str
+    .split(' ').map(word => word.toLowerCase().replace(/^./, word => word.toUpperCase())).join(' ')
+
+
+
+// 6 Copy
+function inserting(arr1, arr2, num) {
+    return [...arr2.slice(0, num), ...arr1, ...arr2.slice(num)]
+}
+
+const arrInserting = (arr1, arr2, num) => [...arr2.slice(0, num), ...arr1, ...arr2.slice(num)]
+
+
+// 7
+function deleteFalsy(arr) {
+    return arr.filter(value => !!value)
+}
+const arrDeleteFalsy = arr => arr.filter(value => !!value)
+
+
+
+// 8
+function isInclude(arr) {
+    const [firstString, secondString] = arr
+    return secondString
+        .split('')
+        .every(letter => new RegExp(letter, 'i').test(firstString))
+}
+
+const arrIsInclude = ([firstString, secondString]) => secondString
+    .split('').every(letter => new RegExp(letter, 'i').test(firstString))
+
+
+// 9
+function matrix(arr, num) {
+    if (arr.length < num) {
+        return arr
+    }
+    return [arr.slice(0, num), ...matrix(arr.slice(num), num)]
+}
+
+const arrMatrix = (arr, num) => arr.length < num ? arr : [arr.slice(0, num), ...arrMatrix(arr.slice(num), num)]
+
+
+// 10
+function recursion(arr, num) {
+    if (num <= 0) {
+        return arr
+    }
+    return [num, ...recursion(arr, num - 1)]
+}
+
+const arrRecursion = (arr, num) => num <= 0 ? arr : [num, ...arrRecursion(arr, num - 1)]
+
+// TESTS
 describe('Factorial', function () {
     function makeTest(number) {
         it(`Факториал ${number} = ${arrFactorial(number)}`, function () {
@@ -39,30 +131,16 @@ describe('Factorial', function () {
     }
 })
 
-
-// 2 longest string
-function longWord(str) {
-    const stringsArr = str.split(' ').sort((a, b) => a.length - b.length)
-    return stringsArr[stringsArr.length - 1].length
-}
-
-const arrLongWord = str => str.split(' ').reduce((accumulate, current) => Math.max(current.length, accumulate), 0)
-
 describe('Longest string', function () {
     it(`Результат ${longWord('one two three Krankenhaus four five six seven')} для 'one two three Krankenhaus four five six seven'`, function () {
         assert.equal(longWord('one two three Krankenhaus four five six seven'), 'Krankenhaus'.length)
     });
     it(`Результат ${arrLongWord('Ausfahrt Eintreten Säule Übersetzer')} для 'Ausfahrt Eintreten Säule Übersetzer'`, function () {
         assert.equal(arrLongWord('Ausfahrt Eintreten Säule Übersetzer'), 'Übersetzer'.length)
+
     });
 })
 
-// 3 biggest num
-function biggestNum(arr) {
-    return arr.map(innerArr => innerArr.reduce((accumulate, current) => Math.max(accumulate, current)))
-}
-
-const arrBiggestNum = arr => arr.map(innerArr => innerArr.reduce((accumulate, current) => Math.max(accumulate, current)))
 
 describe('Biggest number', function () {
     let array = [[1, 2, 3], [4, 5, 6], [7, 8, 10]]
@@ -73,12 +151,6 @@ describe('Biggest number', function () {
         assert.deepEqual(arrBiggestNum(array), [3,6,10])
     })
 })
-
-// 4 string max length
-function stringLength(str, num) {
-    return str.slice(0, num) + '...'
-}
-const arrStringLength = (str, num) => str.slice(0, num) + '...'
 
 describe('String cutter', function () {
     it('Qwert... for "Qwertyui"', function () {
@@ -92,17 +164,6 @@ describe('String cutter', function () {
     });
 })
 
-
-
-// 5 Register
-function changeRegister(str) {
-    return str
-        .split(' ').map(word => word.toLowerCase().replace(/^./, word => word.toUpperCase())).join(' ')
-}
-
-const arrChangeRegister = str => str
-    .split(' ').map(word => word.toLowerCase().replace(/^./, word => word.toUpperCase())).join(' ')
-
 describe('Register changer' , function () {
     it('Hola for hOlA', function () {
         assert.equal(changeRegister('hOlA'), 'Hola')
@@ -112,14 +173,6 @@ describe('Register changer' , function () {
     });
 })
 
-
-// 6 Copy
-function inserting(arr1, arr2, num) {
-    return [...arr2.slice(0, num), ...arr1, ...arr2.slice(num)]
-}
-
-const arrInserting = (arr1, arr2, num) => [...arr2.slice(0, num), ...arr1, ...arr2.slice(num)]
-
 describe('Inserting array into array', function () {
     it('[4,1,2,3,5] for [1,2,3] [4,5]', function () {
         assert.deepEqual(inserting([1,2,3], [4,5], 1), [4,1,2,3,5])
@@ -128,12 +181,6 @@ describe('Inserting array into array', function () {
         assert.deepEqual(arrInserting([1,2,3,4,5,9], [4,5,1,2], 2), [4,5,1,2,3,4,5,9,1,2])
     });
 })
-
-// 7
-function deleteFalsy(arr) {
-    return arr.filter(value => !!value)
-}
-const arrDeleteFalsy = arr => arr.filter(value => !!value)
 
 describe('Deleting falsy elements from array', function () {
     it('[1, "Hi"] for [1, false, "", 0, undefined, "Hi"]', function () {
@@ -147,18 +194,6 @@ describe('Deleting falsy elements from array', function () {
     });
 })
 
-
-// 8
-function isInclude(arr) {
-    const [firstString, secondString] = arr
-    return secondString
-        .split('')
-        .every(letter => new RegExp(letter, 'i').test(firstString))
-}
-
-const arrIsInclude = ([firstString, secondString]) => secondString
-    .split('').every(letter => new RegExp(letter, 'i').test(firstString))
-
 describe('First elem include second elem', function () {
     it('True for ["Hola","aloha"]', function () {
         assert.equal(isInclude(['Hola','aloha']), true)
@@ -170,16 +205,6 @@ describe('First elem include second elem', function () {
         assert.equal(arrIsInclude(['12345','12,34,56']), false)
     });
 })
-
-// 9
-function matrix(arr, num) {
-    if (arr.length < num) {
-        return arr
-    }
-    return [arr.slice(0, num), ...matrix(arr.slice(num), num)]
-}
-
-const arrMatrix = (arr, num) => arr.length < num ? arr : [arr.slice(0, num), ...arrMatrix(arr.slice(num), num)]
 
 describe('Matrix for array', function () {
     it('[[1,2,3],[4,5,6],[7,8,9]] for matrix([1,2,3,4,5,6,7,8,9], 3)', function () {
@@ -193,15 +218,6 @@ describe('Matrix for array', function () {
     });
 })
 
-// 10
-function recursion(arr, num) {
-    if (num <= 0) {
-        return arr
-    }
-    return [num, ...recursion(arr, num - 1)]
-}
-
-const arrRecursion = (arr, num) => num <= 0 ? arr : [num, ...arrRecursion(arr, num - 1)]
 
 describe('recursion for array', function () {
     it('[9,8,7,6,5,4,3,2,1] for recursion([], 9)', function () {
